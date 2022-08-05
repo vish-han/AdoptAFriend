4d977eab7a2ef2565632978c8a454d00ab597b06
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-08-06 00:57:29 +0530

    Project Initialize🚀🚀

---
+++ src/firebase.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzBY6RACHpYMKAe9DSvjTRipy6NbLgOIY",
+  authDomain: "adopt-a-friend-d3b81.firebaseapp.com",
+  projectId: "adopt-a-friend-d3b81",
+  storageBucket: "adopt-a-friend-d3b81.appspot.com",
+  messagingSenderId: "215177113692",
+  appId: "1:215177113692:web:695db5058f3e78ebbe7699"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
